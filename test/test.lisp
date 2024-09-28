538e928e4dd8ccd4c9fcd549e034b361a06a32ae
JenChieh <jcs090218@gmail.com>
2024-09-28 05:37:46 -0700

    confirm test work

--- test/test.lisp
+++ test/test.lisp
@@ -3,6 +3,7 @@
 
 (let ((score (flx:score "buffer-file-name" "bfn")))
   (print score)
+  (terpri)
   (unless (equal score '(237 0 7 12))
-    (print "Error testing `buffer-file-name`")
+    (print (format nil "Error testing `buffer-file-name`: ~a" score))
     (quit :unix-status 1)))
