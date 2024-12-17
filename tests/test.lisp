fccb8b2f2683b319dffda3c097afd1f5b7be58e6
JenChieh <jcs090218@gmail.com>
2024-12-17 01:46:08 -0800

    ensure dev deps

--- tests/test.lisp
+++
@@ -1,8 +0,0 @@
-(load "./flx.lisp")
-
-(let ((score (flx:score "buffer-file-name" "bfn")))
-  (print score)
-  (terpri)
-  (unless (equal score '(237 0 7 12))
-    (print (format nil "Error testing `buffer-file-name`: ~a" score))
-    (quit :unix-status 1)))
